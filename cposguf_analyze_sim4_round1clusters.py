3b3eb5517f857a4e864146ddac97d958a995dbb2
Mark <watermarkhu@gmail.com>
2019-11-12 21:39:18 +0100

    changed sim4 analysis to gaussian variables

--- cposguf_analyze_sim4_round1clusters.py
+++ cposguf_analyze_sim4_round1clusters.py
@@ -1,135 +1,203 @@
 import cposguf_cluster_actions as cca
 from cposguf_run import sql_connection, fetch_query
-import matplotlib.pyplot as plt
-from progiter import ProgIter
 import graph_objects as go
 import toric_error as te
 from collections import defaultdict as dd
+from matplotlib import pyplot as plt
+import scipy.stats as stats
+from scipy.optimize import curve_fit
+import numpy as np
 import pickle
 import os
 
 
 limit = None
-minl, maxl = 1, 3
 plotnum = 25
-maxfetch = 10000
-L = [16 + 4 * i for i in range(8)]
-file = "sim4_r1c_data"
-con, cur = sql_connection()
+maxfetch = 5000
+L = [8] # + 4 * i for i in range(10)]
+P = [(90 + i)/1000 for i in range(21)]
+file = "sim4_r1c_data_gauss"
 
-def save_obj(obj, name):
-    with open(name + '.pkl', 'wb') as f:
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
-def load_obj(name):
-    with open(name + '.pkl', 'rb') as f:
-        return pickle.load(f)
+##############################################
+# definitions:
 
-def d1(): return [0,0]
+def clusters1_3(): # All possible clusters between and including size 1 and 3
+    return 1, 3, [
+        frozenset({(0, 0, 0)}),
+        frozenset({(0, 0, 0), (0, 0, 1)}),
+        frozenset({(0, 0, 1), (1, 0, 1)}),
+        frozenset({(0, 0, 0), (0, 0, 1), (1, 0, 0)}),
+        frozenset({(1, 0, 0), (0, 1, 1), (0, 1, 0)}),
+        frozenset({(1, 0, 0), (0, 0, 1), (1, 0, 1)}),
+        frozenset({(0, 0, 0), (0, 0, 1), (0, 1, 0)}),
+        frozenset({(0, 0, 0), (0, 1, 0), (0, 2, 0)})
+    ]
+
+def d0(): return [0,0]
+def d1(): return [[0,0], [0,0]]
 def d2(): return dd(d1)
 
-if os.path.exists(file + ".pkl"):
+
+def load_obj(name ):
+    with open(f"/data/{name}.pkl", "rb") as f:
+        return pickle.load(f)
+
+def save_obj(obj, name):    # Save object to a pickled file
+    with open(f"/data/{name}.pkl", "wb") as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def get_count(clusters, clist, size):
+    """
+    returns a defaultdict of lists of clusters countaining the tuples of the qubits
+    """
+    count = [0 for i in clist]
+    clusters = cca.listit(clusters)
+
+    # Return to principal cluster
+    for i, cluster in enumerate(clusters):
+        clusters[i] = cca.principal(cluster, ydim=size, xdim=size)
+    for cluster in clusters:
+        augs, cmid = [], []
+        for rot_i in range(4):
+            dim = cca.max_dim(cluster)
+            augs.append(cluster)
+            cmid.append(dim[1])
+            fcluster = frozenset(cluster)
+            if fcluster in clist:
+                count[clist.index(fcluster)] += 1
+                break
+            mcluster = cca.mirror(cluster, dim[0])
+            mdim = cca.max_dim(mcluster)
+            augs.append(mcluster)
+            cmid.append(mdim[1])
+            fmcluster = frozenset(mcluster)
+            if fmcluster in clist:
+                count[clist.index(fmcluster)] += 1
+                break
+            cluster = cca.rotate(cluster, dim[0])
+        else:
+            ftupcluster = frozenset(augs[cmid.index(min(cmid))])
+            count[clist.index(ftupcluster)] += 1
+
+    return count
+
+
+###########################################################
+
+if os.path.exists(file + ".pkl"):           # Load data_base if pickled file exists
     print("loading data " + file)
     data = load_obj(file)
     data_p, data_n = data["data_p"], data["data_n"]
     countp, countn = data["countp"], data["countn"]
-else:
+else:                                       # Initate database
     data_p, data_n = [dd(d2) for _ in range(2)]
-    countp, countn = [dd(d1) for _ in range(2)]
-    cur.execute("SELECT lattice, p, tree_wins, list_wins FROM cases")
-    cases = cur.fetchall()
-    for l, p, tn, ln in cases:
-        countp[(l, float(p))] = [tn, ln]
+    countp, countn = [dd(d0) for _ in range(2)]
 
-for l in L:
-    p = None
+minl, maxl, clist = clusters1_3()
 
-    print("Getting count...")
-    con, cur = sql_connection()
-    cur.execute(fetch_query("COUNT(*)", l, p))
-    num = cur.fetchone()[0]
+for lattice in L:
+    for p in P:
+        temp_p, temp_n = [[], []], dd(list)
 
-    print("Executing query... (be patient)")
-    cur.execute(fetch_query("sim_id, lattice, p, ftree_tlist, seed", l, p))
+        con, cur = sql_connection()
+        print("\nGetting count of L{}, p{}...".format(lattice, p))
+        cur.execute("SELECT tree_wins, list_wins FROM cases WHERE lattice = {} and p = {}".format(lattice, p))
+        tlcount = cur.fetchone()
+        countp[(lattice, p)] = [tlcount[0], tlcount[1]]
 
-    sims = [cur.fetchone()]
+        cur.execute(fetch_query("COUNT(*)", lattice, p))
+        num = cur.fetchone()[0]
+        print("fetching {} simulations...".format(num))
 
-    graph = go.init_toric_graph(l)
-
-    fetched = 1
-    new_data = []
-    while sims != [None]:
-        print("{:0.1f}%".format(fetched/num*100))
-        sims += cur.fetchmany(maxfetch)
-        fetched += maxfetch
-
-        for sim_id, lattice, p, type, seed in sims:
-
-            te.apply_random_seed(seed)
-            te.init_pauli(graph, pX=float(p))
-
-            errors = [
-                graph.E[(0, y, x, td)].qID[1:]
-                for y in range(lattice) for x in range(lattice) for td in range(2)
-                if graph.E[(0, y, x, td)].state
-            ]
-            n = len(errors)
-            new_data.append((n, sim_id))
-
-            # Get clusters from array data
-            clusters = cca.get_clusters_from_list(errors, lattice)
-
-            # Remove single clusters
-            clusters = [
-                cluster
-                for cluster in clusters.values()
-                if len(cluster) >= minl and len(cluster) <= maxl
-            ]
-
-            data_p = cca.get_count2(clusters, data_p, lattice, float(p), type)
-            data_n = cca.get_count2(clusters, data_n, lattice, n, type)
-            countn[(lattice, n)][type] += 1
-
-            graph.reset()
-
+        cur.execute(fetch_query("ftree_tlist, seed", lattice, p))
         sims = [cur.fetchone()]
+        graph = go.init_toric_graph(lattice)
 
-    # print("Don't kill process now. Updating database...")
-    # query = "UPDATE simulations SET n = %s, analysis1 = TRUE WHERE sim_id = %s"
-    #
-    # con, cur = sql_connection()
-    # for this_data in ProgIter(new_data):
-    #     cur.execute(query, this_data)
-    # cur.close()
-    # con.close()
+        fetched = 1
+        while sims != [None]:
 
-    data = {
-        "data_p": data_p,
-        "data_n": data_n,
-        "countp": countp,
-        "countn": countn
-    }
-    save_obj(data, file)
-    print("Process can now be killed")
+            print("{:0.1f}%".format(fetched/num*100))
+            sims += cur.fetchmany(maxfetch)
+            fetched += maxfetch
 
-cur.close()
-con.close()
+            for type, seed in sims:
 
+                # Get errors from seed
+                te.apply_random_seed(seed)
+                te.init_pauli(graph, pX=float(p))
+                errors = [
+                    graph.E[(0, y, x, td)].qID[1:]
+                    for y in range(lattice) for x in range(lattice) for td in range(2)
+                    if graph.E[(0, y, x, td)].state
+                ]
+                n = len(errors)
 
-# sorted(cluster_data.items(), key=lambda kv: sum(kv[1]), reverse=True)
+                # Get clusters from array data
+                clusters = cca.get_clusters_from_list(errors, lattice)
 
-# fig = cca.plot_clusters(clusters, count, plotnum)
-#
-# folder = "../../../OneDrive - Delft University of Technology/MEP - thesis Mark/Simulations/cposguf_sim3_round1clusters/"
-# file_name = "cposguf_sim3_L-"
-# file_name += "None_p-" if l is None else "{0:d}_p-".format(l)
-# file_name += "None" if p is None else "{0:.3f}".format(p)
-# fname = folder + "figures/" + file_name + ".pdf"
-# fig.savefig(fname, transparent=True, format="pdf", bbox_inches="tight")
-# plt.close(fig)
-#
-# f = open(folder + "data/" + file_name + ".txt", "w")
-# f.write("Count: " + str(count))
-# for line in clusters:
-#     f.write(str(line) + "\n")
-# f.close()
+                # Remove outlier clusters
+                clusters = [
+                    cluster
+                    for cluster in clusters.values()
+                    if len(cluster) >= minl and len(cluster) <= maxl
+                ]
+
+                # Store cluster count to temp dictionaries
+                count = get_count(clusters, clist, lattice)
+                temp_p[type].append(count)
+                temp_n[(n, type)].append(count)
+
+                graph.reset()
+            sims = [cur.fetchone()]
+        else:
+            print("100%")
+            cur.close()
+            con.close()
+
+        # Get mean and std of p data distribution and store to database
+        cl0 = list(map(list, zip(*temp_p[0])))
+        cl1 = list(map(list, zip(*temp_p[1])))
+
+        for list0, list1, key in zip(cl0, cl1, clist):
+            data_p[key][(lattice, p)][0] = [np.mean(list0), np.var(list0)]
+            data_p[key][(lattice, p)][1] = [np.mean(list1), np.var(list1)]
+
+        # Get mean and std of n data distribution and combine with existing mean and std of previous analysis
+
+        # (n, type), nlists = sorted(list(temp_n.items()), key=lambda kv: len(kv[1]), reverse=True)[0]
+        # oc = countp[(lattice, p)][type]
+        # nc = len(nlists)
+        # cl = list(map(list, zip(*nlists)))
+        # fig = plt.figure()
+        # plt.hist(cl[0], bins=range(min(cl[0]), max(cl[0]) + 1, 1), normed=True)
+        # x_axis = np.arange(min(cl[0]), max(cl[0]), 0.01)
+        # plt.plot(x_axis, stats.norm.pdf(x_axis,np.mean(cl[0]),np.std(cl[0])))
+        # plt.xlabel("Population on lattice")
+        # plt.ylabel("Normalized occurance")
+        # name = f"Occurance of R1-cluster 0 in L{lattice}, n{n}"
+        # plt.title(name)
+        # plt.show()
+        # fig.savefig(name + ".pdf", transparent=True, format="pdf", bbox_inches="tight")
+        # exit()
+
+        for (n, type), nlists in temp_n.items():
+            oc = countp[(lattice, p)][type]
+            nc = len(nlists)
+            cl = list(map(list, zip(*nlists)))
+
+            for list0, key in zip(cl, clist):
+                oldmu, oldva = data_n[key][(lattice, n)][type]
+                newmu = (oc*oldmu + nc*np.mean(list0))/(oc+nc)
+                newva = (oc*(oldmu**2 + oldva) + nc*(newmu**2 + np.var(list0)))/(oc+nc) - newmu**2
+                data_n[key][(lattice, n)][type] = [newmu, newva]
+            countn[(lattice, n)][type] += nc
+
+        print("Saving data...")
+        data = {    # Save to single data file
+            "data_p": data_p,
+            "data_n": data_n,
+            "countp": countp,
+            "countn": countn
+        }
+        save_obj(data, file)
--- cposguf_analyze_sim4_round1clusters_plot.py
+++ cposguf_analyze_sim4_round1clusters_plot.py
@@ -2,11 +2,20 @@ import pickle
 from collections import defaultdict as dd
 import cposguf_cluster_actions as cca
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+import matplotlib as mpl
+import os
+import numpy as np
+mpl.rcParams["savefig.directory"] = os.chdir(os.getcwd())
 
 
 def load_obj(name ):
-    with open(name + '.pkl', 'rb') as f:
+    with open(f"/data/{name}.pkl", "rb") as f:
         return pickle.load(f)
+
+def save_obj(obj, name):    # Save object to a pickled file
+    with open(f"/data/{name}.pkl", "wb") as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 def clear_ax(i, n, name=""):
     ax = plt.gca()
@@ -22,89 +31,148 @@ def clear_ax(i, n, name=""):
         ax.set_xlabel(name)
 
 
-def d1(): return [0,0]
+def d0(): return [0,0]
+def d1(): return [[0,0], [0,0]]
 def d2(): return dd(d1)
 
-data = load_obj("sim4_r1c_data")
+
+data = load_obj("sim4_r1c_data_gauss12_44")
+lrange = [8, 12, 16, 20, 24, 28, 32, 36, 40, 44]
+prange = [(90 + i)/1000 for i in range(21)]
+plotn = [0 + i for i in range(8)]
+
+####################################
 
 def sort_data(data, type=None):
     if type == None:
         sortdata = sorted(data.items(),
             key=lambda kv:
-            sum([v1 + v2 for v1, v2 in list(kv[1].values())]),
+            sum([v1[0] + v2[0] for v1, v2 in list(kv[1].values())]),
             reverse = True
         )
     elif type in data:
         sortdata = sorted(data.items(),
             key=lambda kv:
-            sum(kv[type]),
+            sum([v[type][0] for v in list(kv[1].values())]),
             reverse = True
         )
     else:
         raise(KeyError)
     return sortdata
 
-
 sdp = sort_data(data["data_p"])
 snp = sort_data(data["data_n"])
-
-len(sdp)
-
 countn, countp = data["countn"], data["countp"]
 
-L = [8, 12]
-LS = ["-", ":", "--"]
-plotn=[0 + i for i in range(8)]
 
-grid = plt.GridSpec(len(plotn), 5, wspace=0.4, hspace=0.3)
+################################
+
+fig0 = plt.figure()
+grid = plt.GridSpec(5, len(plotn), wspace=0.1, hspace=1)
+for i, cnum in enumerate(plotn):
+    ax = plt.subplot(grid[0, i])
+    cca.plot_cluster(sdp[i][0], 2, 2, ax=ax)
+    ax.set_title(str(cnum))
+
+ax = plt.subplot(grid[1:, :])
+
+clms = np.zeros((2,len(plotn)))
+clmt = np.zeros((len(lrange), len(prange), len(plotn)))
+
+for j, l in enumerate(lrange):
+    for k, p in enumerate(prange):
+        oc, (nc0, nc1) = 0, countp[(l, p)]
+        (no0, _), (no1, _) = sdp[plotn[0]][1][(l, p)]
+        for i, cnum in enumerate(plotn):
+            omu, ova = clms[0][i], clms[1][i]
+            (mu0, va0), (mu1, va1) = sdp[cnum][1][(l, p)]
+            clms[0][i] = (oc*omu + nc0*mu0/no0 + nc1*mu1/no1)/(oc+nc0+nc1)
+            clms[1][i] = (oc*(omu**2 + ova) + nc0*(mu0**2 + va0)/no0**2 + nc1*(mu1**2 + va1)/no1**2)/(oc + nc0 + nc1) - clms[0][i]**2
+            clmt[j, k, i] = (nc0*mu0/no0 + nc1*mu1/no1)/(nc0+nc1)
+        oc += nc0 + nc1
+
+ax.set_title("Normalized averaged occurance rate of R1-clusters")
+ax.set_xlabel("Cluster #")
+ax.set_ylabel("Occurance rate")
+plt.errorbar(plotn, clms[0], np.sqrt(clms[1]), elinewidth=1, capsize=2, color="k", ls=":")
+plt.show()
+
+
+################################
+
+rtmt = np.zeros((len(lrange), len(prange), len(plotn)))
+fig = plt.figure()
+grid = plt.GridSpec(len(plotn), 10, wspace=0.8, hspace=0.5)
 
 for i, cnum in enumerate(plotn):
 
-    ax = plt.subplot(grid[i, 0])
-    cca.plot_cluster(sdp[i][0], 3, 3, ax=ax)
-    ax0=plt.subplot(grid[i, 1:3])
-    ax0.axhline(y=1, color="k", ls="--")
-    clear_ax(i, len(plotn), "p")
-    ax1=plt.subplot(grid[i, 3:])
-    ax1.axhline(y=1, color="k", ls="--")
-    clear_ax(i, len(plotn), "norm n")
+    # ax = plt.subplot(grid[i, 0])
+    # ax.set_ylabel(str(i))
+    # cca.plot_cluster(sdp[i][0], 2, 2, ax=ax)
+    # ax0 = plt.subplot(grid[i, 1:4])
+    # plt.ylim(0.9, 1.1)
+    # ax0.axhline(y=1, color="k", ls="--", lw=0.5)
+    # clear_ax(i, len(plotn), "p")
+    # ax1 = plt.subplot(grid[i, 4:7])
+    # plt.ylim(0.75, 1.25)
+    # ax1.axhline(y=1, color="k", ls="--", lw=0.5)
+    # clear_ax(i, len(plotn), "n")
+    # ax2 = plt.subplot(grid[i, 7:])
+    # plt.ylim(0.75, 1.25)
+    # ax2.axhline(y=1, color="k", ls="--", lw=0.5)
+    # clear_ax(i, len(plotn), "norm n")
 
+    for j, l, in enumerate(lrange):
+        color="C{}".format(j % 10)
 
-    for i, l, in enumerate(L):
-        color="C{}".format(i % 10)
-
-        prange = sorted([p for _,p in sdp[cnum][1].keys()])
-        nrange = sorted([n for _,n in snp[cnum][1].keys()])
-
-        pratio, prange2, perr = [], [], []
-        for p in prange:
-            rt, rl = sdp[cnum][1][(l, p)]
+        pratio, prange2, pavgc = [], [], []
+        for k, p in enumerate(prange):
+            (mt, vt), (ml, vl) = sdp[cnum][1][(l, p)]
             nt, nl = countp[(l, p)]
-            pt, pl = rt/nt, rl/nl
-            var = abs(((1-pt)/(nt*pt) + (1-pl)/(nl*pl))**(1/2)*nt*pt/(nl*pl))
             prange2.append(p*100)
-            pratio.append(pt/pl)
-            perr.append(var)
+            pratio.append(mt/ml)
+            pavgc.append((nt + nl)/2)
+            rtmt[j, k, i] = mt/ml
 
-        nratio, nrange2, nerr = [], [], []
+        nrange = sorted([n for _,n in snp[cnum][1].keys()])
+        nratio, nrange2, navgc = [], [], []
         for n in nrange:
-            rt, rl = snp[cnum][1][(l, n)]
+            (mt, vt), (ml, vl) = snp[cnum][1][(l, n)]
             nt ,nl = countn[(l, n)]
-            if rt > 500 and nt > 1000:
-                pt, pl = rt/nt, rl/nl
-                var = abs(((1-pt)/(nt*pt) + (1-pl)/(nl*pl))**(1/2)*nt*pt/(nl*pl))
+            if mt > 0 and ml > 0 and nt> 500:
                 nrange2.append(n)
-                nratio.append(pt/pl)
-                nerr.append(var)
+                nratio.append(mt/ml)
+                navgc.append((nt + nl)/2)
 
         nrange3 = []
         for n in nrange2:
             nrange3.append((n-nrange2[0])/(nrange2[-1] - nrange2[0]))
 
-        ax0.errorbar(prange2, pratio, perr, color=color, ls=":", elinewidth=0.5, capsize=1)
-        ax1.errorbar(nrange3, nratio, nerr, color=color, ls=":", elinewidth=0.5, capsize=1)
+        # for prn, prt, pac in zip(prange2, pratio, pavgc):
+        #     ax0.scatter(prn, prt, 1.5, color=color, alpha=pac/max(pavgc))
 
+        # for nrn0, nrn1, nrt, nac in zip(nrange2, nrange3, nratio, navgc):
+        #     ax1.scatter(nrn0, nrt, 1.5, color=color, alpha=nac/max(navgc))
+        #     ax2.scatter(nrn1, nrt, 1.5, color=color, alpha=nac/max(navgc))
+        #
+        # ax0.plot(prange2, pratio, color=color, ls=":")
+
+        # ax1.plot(nrange2, nratio, color=color, ls=":")
+        # ax2.plot(nrange3, nratio, color=color, ls=":")#, alpha = 1-0.2*i)
+
+
+custom_lines = [Line2D([0], [0], color="C{}".format(i % 10), label=str(c)) for i, c in enumerate(lrange)]
+fig.legend(handles=custom_lines, bbox_to_anchor=(0.97, 0.55))
 plt.show()
+
+
+tldata = {
+    "tl_ratio_p": rtmt,
+    "norm_avg_occ_p": clmt
+    }
+
+save_obj(tldata, "sim4_tldata")
+
 
 # fig = cca.plot_clusters(clusters, count, plotnum)
 #

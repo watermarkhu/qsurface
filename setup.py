01c3a98f50bb43467f7f3ff1742a310aafbc2823
Mark <watermarkhu@gmail.com>
2020-07-16 21:59:18 +0200

    added setuptools setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='opensurfacesim',
+      version=0.1,
+      description="Open library from surface code simulations and visualizations",
+      url="https://github.com/watermarkhu/opensurfacesim",
+      author="Mark Shui Hu",
+      author_email="watermarkhu@gmail.com",
+      license="BSD-3",
+      packages=["opensurfacesim"],
+      zip_safe=False)
